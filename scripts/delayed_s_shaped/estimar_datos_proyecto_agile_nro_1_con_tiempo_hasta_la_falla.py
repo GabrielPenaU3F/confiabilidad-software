5cb4bb75dce6b8daf94dcd14fdf791e3f689416a
Gabriel Pena <gpena@untref.edu.ar>
2019-07-08 22:51:46 -0300

    Corrijo condiciones iniciales y agrego comentarios del caso

--- scripts/delayed_s_shaped/estimar_datos_proyecto_agile_nro_1_con_tiempo_hasta_la_falla.py
+++ scripts/delayed_s_shaped/estimar_datos_proyecto_agile_nro_1_con_tiempo_hasta_la_falla.py
@@ -14,12 +14,21 @@ ds = EstimadorDelayedSShaped()
 
 params_ds_lsq = ds.ajustar_numero_medio_de_fallas_por_minimos_cuadrados(ttf, fallas_acumuladas)
 
-params_ds_mv = None
+'''
+        Lo que ocurre acá es insólito. Para empezar, la resolución resulta exremadamente sensible a las condiciones 
+        iniciales. Poniendo algún valor ligeramente distinto, a veces te halla una solución rarísima 
+        (me han aparecido, por ejemplo, valores de a negativos que daban curvas decrecientes). Como él ajuste de 
+        mínimos cuadrados no converge, no se lo puede usar como input inicial, provoca que las iteraciones no lo muevan
+        de ahí, es decir MV tampoco converge en ese caso. Cambiando a mano los parametros inciales di con varias 
+        combinaciones que dan curvas mas o menos cercanas: [30, 0.003], [30, 0.005], [27, 0.003] por ejemplo. Pero, 
+        si le varias de 30 a 31 ya te da cualquier cosa. Y muchas veces directamente encuentra overflow y corta. 
+        Muy raro. Me pasó solamente con estos datos, los del NDTS andan barbaro. Tomar la parte inicial no sirve,
+        son muy poquitos datos. 
 '''
 params_ds_mv = ds.estimar_parametros_por_maxima_verosimilitud_tiempo_hasta_la_falla(ttf[1:], fallas_acumuladas[-1],
-                                                                                    params_ds_lsq,
+                                                                                    [30, 0.003],
                                                                                     metodo_resolucion='krylov')
-'''
+
 
 fig, ax = plt.subplots()
 
