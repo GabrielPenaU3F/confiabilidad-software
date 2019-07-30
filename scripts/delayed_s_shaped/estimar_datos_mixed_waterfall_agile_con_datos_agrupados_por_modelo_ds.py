a4c73498517d88a0c3882fc20e7989ab032f47d6
Gabriel Pena <gpena@untref.edu.ar>
2019-07-29 22:30:34 -0300

    Refactor, parte 5: Reescritas todas las ecuaciones del modelo DS con datos agrupados, implementadas las nuevas y corregido el script.

--- scripts/delayed_s_shaped/estimar_datos_mixed_waterfall_agile_con_datos_agrupados_por_modelo_ds.py
+++ scripts/delayed_s_shaped/estimar_datos_mixed_waterfall_agile_con_datos_agrupados_por_modelo_ds.py
@@ -12,10 +12,13 @@ fallas_acumuladas = datos_fpd.get_fallas_acumuladas()
 ds = EstimadorDelayedSShaped()
 
 aprox_inicial = (1, 0.5)
-params_ds_lsq = ds.ajustar_numero_medio_de_fallas_por_minimos_cuadrados(dias, fallas_acumuladas, aprox_inicial)
-
-params_ds_mv = ds.estimar_parametros_por_maxima_verosimilitud_fallas_por_dia(
-    dias, fallas_por_dia, params_ds_lsq, metodo_resolucion='krylov')
+params_ds_mc = ds.ajustar_numero_medio_de_fallas_por_minimos_cuadrados(dias, fallas_acumuladas, aprox_inicial)
+params_ds_mv_fallas_por_dia = ds.\
+    estimar_parametros_por_maxima_verosimilitud_fallas_por_dia(dias, fallas_por_dia, params_ds_mc,
+                                                               metodo_resolucion='krylov')
+params_ds_mv_fallas_acumuladas_al_dia = ds.\
+    estimar_parametros_por_maxima_verosimilitud_fallas_acumuladas_al_dia(dias, fallas_acumuladas, params_ds_mc,
+                                                                         metodo_resolucion='krylov')
 
 fig, ax = plt.subplots()
 
@@ -31,22 +34,40 @@ ax.grid(color='black', linestyle='--', linewidth=0.5)
 
 ax.plot(dias, fallas_acumuladas, linewidth=1, color='#263859', linestyle='--',
         label='Datos reales (Mixed Waterfall-Agile)')
-ax.plot(dias, ds.calcular_numero_medio_de_fallas(dias, params_ds_lsq[0], params_ds_lsq[1]),
-        linewidth=1, color='#ca3e47', linestyle='-', label='LSQ: a=%.5f, b=%.5f' % tuple(params_ds_lsq))
-if params_ds_mv is not None:
-    ax.plot(dias, ds.calcular_numero_medio_de_fallas(dias, params_ds_mv[0], params_ds_mv[1]),
-            linewidth=1, color='#58b368', linestyle='-', label='MV: a=%.5f, b=%.5f' % tuple(params_ds_mv))
+ax.plot(dias, ds.calcular_numero_medio_de_fallas(dias, params_ds_mc[0], params_ds_mc[1]),
+        linewidth=1, color='#ca3e47', linestyle='-', label='Mínimos cuadrados')
+ax.plot(dias, ds.calcular_numero_medio_de_fallas(dias, params_ds_mv_fallas_acumuladas_al_dia[0],
+                                                 params_ds_mv_fallas_acumuladas_al_dia[1]),
+        linewidth=1, color='#1b7fbd', linestyle='-', label='Máxima verosimilitud (Acum)')
+ax.plot(dias, ds.calcular_numero_medio_de_fallas(dias, params_ds_mv_fallas_por_dia[0],  params_ds_mv_fallas_por_dia[1]),
+        linewidth=1, color='#58b368', linestyle='-', label='Máxima verosimilitud (FPD)')
 
 ax.legend()
 
 ax.plot()
 
-prr_lsq = ds.calcular_prr(dias, fallas_acumuladas, params_ds_lsq[0], params_ds_lsq[1])
-prr_mv = ds.calcular_prr(dias, fallas_acumuladas, params_ds_mv[0], params_ds_mv[1])
-print(Fore.GREEN + ('PRR - LSQ: ' + prr_lsq.__str__()))
-print(Fore.GREEN + ('PRR - MV: ' + prr_mv.__str__()))
+print(Fore.BLUE + ('a = ' + params_ds_mc[0].__str__() + ' (Mínimos cuadrados)'))
+print(Fore.BLUE + ('b = ' + params_ds_mc[1].__str__() + ' (Mínimos cuadrados)'))
+print(Fore.BLUE + ('a = ' + params_ds_mv_fallas_acumuladas_al_dia[0].__str__() +
+                   '(Máxima verosimilitud, fallas acumuladas)'))
+print(Fore.BLUE + ('b = ' + params_ds_mv_fallas_acumuladas_al_dia[1].__str__() +
+                   '(Máxima verosimilitud, fallas acumuladas)'))
+print(Fore.BLUE + ('a = ' + params_ds_mv_fallas_por_dia[0].__str__() + ' (Máxima verosimilitud, fallas por día)'))
+print(Fore.BLUE + ('b = ' + params_ds_mv_fallas_por_dia[1].__str__() + ' (Máxima verosimilitud, fallas por día)'))
 
-aic_mv = ds.calcular_aic_fallas_por_dia(dias, fallas_por_dia, params_ds_mv[0], params_ds_mv[1])
-print(Fore.GREEN + ('AIC (FPD): ' + aic_mv.__str__()))
+prr_mc = ds.calcular_prr(dias, fallas_acumuladas, params_ds_mc[0], params_ds_mc[1])
+prr_mv_facum = ds.calcular_prr(dias, fallas_acumuladas, params_ds_mv_fallas_acumuladas_al_dia[0],
+                               params_ds_mv_fallas_acumuladas_al_dia[1])
+prr_mv_fpd = ds.calcular_prr(dias, fallas_acumuladas, params_ds_mv_fallas_por_dia[0], params_ds_mv_fallas_por_dia[1])
+print(Fore.GREEN + ('PRR - Mínimos cuadrados: ' + prr_mc.__str__()))
+print(Fore.GREEN + ('PRR - Máxima verosimilitud (Fallas acumuladas): ' + prr_mv_facum.__str__()))
+print(Fore.GREEN + ('PRR - Máxima verosimilitud (Fallas por día): ' + prr_mv_fpd.__str__()))
+
+aic_mv_facum = ds.calcular_aic_fallas_acumuladas_al_dia(dias, fallas_acumuladas, params_ds_mv_fallas_por_dia[0],
+                                                        params_ds_mv_fallas_por_dia[1])
+aic_mv_fpd = ds.calcular_aic_fallas_por_dia(dias, fallas_por_dia, params_ds_mv_fallas_por_dia[0],
+                                            params_ds_mv_fallas_por_dia[1])
+print(Fore.GREEN + ('AIC (Fallas acumuladas): ' + aic_mv_facum.__str__()))
+print(Fore.GREEN + ('AIC (Fallas por día): ' + aic_mv_fpd.__str__()))
 
 plt.show()
--- src/modelos/delayed_s_shaped/estimador_delayed_s_shaped.py
+++ src/modelos/delayed_s_shaped/estimador_delayed_s_shaped.py
@@ -33,10 +33,23 @@ class EstimadorDelayedSShaped(EstimadorModelo):
                 self.ecuacion_mv_2_fallas_acumuladas_al_dia(a, b, dias, fallas_acumuladas_al_dia))
 
     def ecuacion_mv_1_fallas_acumuladas_al_dia(self, a, b, dias, fallas_acumuladas_al_dia):
-        pass
+        suma_yi = np.sum(fallas_acumuladas_al_dia)
+        suma_mu_ti = 0
+        for i in range(len(dias)):
+            t_i = dias[i]
+            mu_ti = self.calcular_media(t_i, a, b)
+            suma_mu_ti += mu_ti
+        return suma_yi - suma_mu_ti
 
     def ecuacion_mv_2_fallas_acumuladas_al_dia(self, a, b, dias, fallas_acumuladas_al_dia):
-        pass
+        sumatoria = 0
+        for i in range(len(dias)):
+            t_i = dias[i]
+            y_i = fallas_acumuladas_al_dia[i]
+            mu_ti = self.calcular_media(t_i, a, b)
+            corchete = (y_i / mu_ti) - 1
+            sumatoria += ((t_i**2) * self.calcular_exp_menos_bt(b, t_i) * corchete)
+        return sumatoria
 
     def ecuaciones_mv_fallas_por_dia(self, dias, fallas_por_dia, vec):
         a, b = vec
@@ -44,24 +57,36 @@ class EstimadorDelayedSShaped(EstimadorModelo):
                 self.ecuacion_mv_2_fallas_por_dia(a, b, dias, fallas_por_dia))
 
     def ecuacion_mv_1_fallas_por_dia(self, a, b, dias, fallas_por_dia):
-        fallas_acumuladas_al_dia = self.calcular_fallas_acumuladas(fallas_por_dia)
-        suma_yi = np.sum(fallas_acumuladas_al_dia)
-        segundo_termino = 0
-        for i in range(len(dias)):
-            t_i = dias[i]
-            segundo_termino += (1 - (1 + b * t_i) * np.exp(-b * t_i))
-        return suma_yi/a - segundo_termino
+        deltas_yi = fallas_por_dia
+        suma_delta_yi = np.sum(deltas_yi)
+        t_n = dias[-1]
+        mu_tn = self.calcular_media(t_n, a, b)
+        return suma_delta_yi - mu_tn
 
     def ecuacion_mv_2_fallas_por_dia(self, a, b, dias, fallas_por_dia):
-        fallas_acumuladas_al_dia = self.calcular_fallas_acumuladas(fallas_por_dia)
-        suma = 0
-        for i in range(len(dias)):
+        deltas_yi = fallas_por_dia
+        sumatoria = 0
+        for i in range (len(dias)):
             t_i = dias[i]
-            yi = fallas_acumuladas_al_dia[i]
-            corchete = (yi / (1 - (1 + b * t_i) * np.exp(-b * t_i)) - a)
-            suma += (t_i ** 2) * np.exp(-b * t_i) * corchete
-        return b * suma
+            if i == 0:
+                t_i_menos_1 = 0
+            else:
+                t_i_menos_1 = dias[i - 1]
+            delta_yi = deltas_yi[i]
+            sumatoria += delta_yi * self.calcular_psi(b, t_i, t_i_menos_1) / self.calcular_phi(b, t_i, t_i_menos_1)
+        t_n = dias[-1]
+        return b * sumatoria - a * (1 + b * (t_n**2) * self.calcular_exp_menos_bt(b, t_n))
 
     def calcular_exp_menos_bt(self, b, t):
         return np.exp(-b * t)
+
+    def calcular_phi(self, b, t_1, t_2):
+        primer_termino = (1 + b*t_2) * self.calcular_exp_menos_bt(b, t_2)
+        segundo_termino = (1 + b*t_1) * self.calcular_exp_menos_bt(b, t_1)
+        return primer_termino - segundo_termino
+
+    def calcular_psi(self, b, t_1, t_2):
+        primer_termino = (t_1**2) * self.calcular_exp_menos_bt(b, t_1)
+        segundo_termino = (t_2**2) * self.calcular_exp_menos_bt(b, t_2)
+        return primer_termino - segundo_termino
 
