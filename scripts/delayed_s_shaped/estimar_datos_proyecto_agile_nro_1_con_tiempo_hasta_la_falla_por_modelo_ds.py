3d48b9b60635fb0ebb50cafbcb1c54ce0ebd46e6
Gabriel Pena <gpena@untref.edu.ar>
2019-07-29 22:10:16 -0300

    Actualizo el formato de script DS (proyecto Agile #1)

--- scripts/delayed_s_shaped/estimar_datos_proyecto_agile_nro_1_con_tiempo_hasta_la_falla_por_modelo_ds.py
+++ scripts/delayed_s_shaped/estimar_datos_proyecto_agile_nro_1_con_tiempo_hasta_la_falla_por_modelo_ds.py
@@ -10,14 +10,16 @@ inicio = 0
 fin = datos.get_cantidad_datos()
 ttf = datos.get_tiempos_de_falla()[inicio:fin]
 fallas_acumuladas = datos.get_fallas_acumuladas()[inicio:fin]
+n_fallas = fallas_acumuladas[-1]
+ttf_sin_cero = ttf[1:]
 
 ds = EstimadorDelayedSShaped()
 
 aprox_inicial = (1, 0.5)
-params_ds_lsq = ds.ajustar_numero_medio_de_fallas_por_minimos_cuadrados(ttf, fallas_acumuladas, aprox_inicial)
+params_ds_mc = ds.ajustar_numero_medio_de_fallas_por_minimos_cuadrados(ttf, fallas_acumuladas, aprox_inicial)
 
-params_ds_mv = ds.estimar_parametros_por_maxima_verosimilitud_tiempo_hasta_la_falla(ttf[1:], fallas_acumuladas[-1],
-                                                                                    params_ds_lsq,
+params_ds_mv = ds.estimar_parametros_por_maxima_verosimilitud_tiempo_hasta_la_falla(ttf_sin_cero, n_fallas,
+                                                                                    params_ds_mc,
                                                                                     metodo_resolucion='krylov')
 
 
@@ -34,22 +36,28 @@ ax.set_facecolor("#ffffff")
 ax.grid(color='black', linestyle='--', linewidth=0.5)
 
 ax.plot(ttf, fallas_acumuladas, linewidth=1, color='#263859', linestyle='--', label='Datos reales (Agile #1)')
-ax.plot(ttf, ds.calcular_numero_medio_de_fallas(ttf, params_ds_lsq[0], params_ds_lsq[1]),
-        linewidth=1, color='#ca3e47', linestyle='-', label='LSQ: a=%.5f, b=%.5f' % tuple(params_ds_lsq))
-if params_ds_mv is not None:
-    ax.plot(ttf, ds.calcular_numero_medio_de_fallas(ttf, params_ds_mv[0], params_ds_mv[1]),
-            linewidth=1, color='#58b368', linestyle='-', label='MV: a=%.5f, b=%.5f' % tuple(params_ds_mv))
-
+ax.plot(ttf, ds.calcular_numero_medio_de_fallas(ttf, params_ds_mc[0], params_ds_mc[1]),
+        linewidth=1, color='#ca3e47', linestyle='-', label='Mínimos cuadrados')
+ax.plot(ttf, ds.calcular_numero_medio_de_fallas(ttf, params_ds_mv[0], params_ds_mv[1]),
+        linewidth=1, color='#58b368', linestyle='-', label='Máxima verosimilitud (Tiempo hasta la falla)')
 ax.legend()
 
 ax.plot()
 
-prr_lsq = ds.calcular_prr(ttf, fallas_acumuladas, params_ds_lsq[0], params_ds_lsq[1])
-prr_mv = ds.calcular_prr(ttf, fallas_acumuladas, params_ds_mv[0], params_ds_mv[1])
-print(Fore.GREEN + ('PRR - LSQ: ' + prr_lsq.__str__()))
-print(Fore.GREEN + ('PRR - MV: ' + prr_mv.__str__()))
+print(Fore.BLUE + ('a = ' + params_ds_mc[0].__str__() + ' (Mínimos cuadrados)'))
+print(Fore.BLUE + ('b = ' + params_ds_mc[1].__str__() + ' (Mínimos cuadrados)'))
+print(Fore.BLUE + ('a = ' + params_ds_mv[0].__str__() +
+                   '(Máxima verosimilitud, tiempo hasta la falla)'))
+print(Fore.BLUE + ('b = ' + params_ds_mv[1].__str__() +
+                   '(Máxima verosimilitud, tiempo hasta la falla)'))
 
-aic_mv = ds.calcular_aic_tiempo_hasta_la_falla(ttf, fallas_acumuladas[-1], params_ds_mv[0], params_ds_mv[1])
-print(Fore.GREEN + ('AIC (TTF): ' + aic_mv.__str__()))
+prr_mc = ds.calcular_prr(ttf, fallas_acumuladas, params_ds_mc[0], params_ds_mc[1])
+prr_mv = ds.calcular_prr(ttf, fallas_acumuladas, params_ds_mv[0], params_ds_mv[1])
+print(Fore.GREEN + ('PRR - Mínimos cuadrados: ' + prr_mc.__str__()))
+print(Fore.GREEN + ('PRR - Máxima verosimilitud: ' + prr_mv.__str__()))
+
+aic_mv = ds.calcular_aic_tiempo_hasta_la_falla(ttf_sin_cero, n_fallas,  params_ds_mv[0], params_ds_mv[1])
+print(Fore.GREEN + ('AIC (Tiempo hasta la falla): ' + aic_mv.__str__()))
 
 plt.show()
+
