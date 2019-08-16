0468a7ca8f13397f7ba2b8c278a5a8ba59e9cef8
Gabriel Pena <gpena@untref.edu.ar>
2019-08-16 01:55:09 -0300

    Corrijo plotters

--- src/plotter/plot_strategy/goel_okumoto_plot_strategy.py
+++ src/plotter/plot_strategy/goel_okumoto_plot_strategy.py
@@ -11,13 +11,14 @@ class GoelOkumotoPlotStrategy(PlotStrategy):
         super().__init__(project_name)
 
     def plot(self, project_name, lsq_params, ml_params):
+        project_title = DataRepository.provide_project_data(project_name).get_project_title()
         x_axis_data = self.data.get_data()
         cumulative_failures = self.data.get_cumulative_failures()
         go = GoelOkumotoEstimator()
 
         fig, ax = plt.subplots()
         ax.plot(x_axis_data, cumulative_failures, linewidth=1, color='#263859', linestyle='--',
-                label='Real data (' + project_name + ')')
+                label='Real data (' + project_title + ')')
         ax.plot(x_axis_data, go.calculate_mean_failure_numbers(x_axis_data, lsq_params[0], lsq_params[1]),
                 linewidth=1, color='#ca3e47', linestyle='-', label='Least squares')
         ax.plot(x_axis_data, go.calculate_mean_failure_numbers(x_axis_data, ml_params[0], ml_params[1]),
