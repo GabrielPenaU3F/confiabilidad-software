7fc59dd107daededd7d3b03499bad8ba908b14ea
Gabriel Pena <gpena@untref.edu.ar>
2019-08-26 01:16:37 -0300

    Testeo la estimacion de minimos cuadrados (NTDS - Goel Okumoto)

--- test/models/goel_okumoto_tests_with_ntds_data.py
+++ test/models/goel_okumoto_tests_with_ntds_data.py
@@ -18,3 +18,11 @@ class GoelOkumotoTestsWithNtdsData(unittest.TestCase):
     def test_ntds_goel_okumoto_maximum_likelihood_b_parameter_is_0_comma_0057(self):
         b = GoelOkumotoTestsWithNtdsData.fit.get_ml_parameters()[1]
         self.assertAlmostEqual(b, 0.005790, places=6)
+
+    def test_ntds_goel_okumoto_least_squares_a_parameter_is_33_comma_59(self):
+        a = GoelOkumotoTestsWithNtdsData.fit.get_lsq_parameters()[0]
+        self.assertAlmostEqual(a, 33.599359, places=6)
+
+    def test_ntds_goel_okumoto_least_squares_b_parameter_is_0_comma_0062(self):
+        b = GoelOkumotoTestsWithNtdsData.fit.get_lsq_parameters()[1]
+        self.assertAlmostEqual(b, 0.006296, places=6)
