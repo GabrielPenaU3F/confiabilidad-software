9b242f9333f12d4952467a4c1b6b84bfb842b08f
Gabriel Pena <gpena@untref.edu.ar>
2019-08-26 02:33:32 -0300

    Agrego archivo de test de logistico

---
+++ test/models/logistic/logistic_tests_with_grouped_failures_per_day_mixed_waterfall_agile.py
@@ -0,0 +1,15 @@
+import unittest
+
+from src.fitters.fitter import GroupedFailuresPerDayFitter
+
+
+class LogisticTestsWithGroupedFailuresPerDayFormatMixedWaterfallAgileData(unittest.TestCase):
+
+    fit = None
+
+    @classmethod
+    def setUpClass(cls):
+        cls.fit = GroupedFailuresPerDayFitter().fit('logistic', 'mixed-waterfall-agile')
+
+    # No se puede testear, no ajusta el metodo
+    # Todo: revisar y corregir el problema si es posible
