68f42995d3590191f0fe83dc7820685cb3146ecb
Gabriel Pena <gpena@untref.edu.ar>
2019-08-26 02:26:01 -0300

    Testeo la estimacion completa (Mixed Agile-Waterfall - FPD - Goel Okumoto)

---
+++ test/models/goel_okumoto/goel_okumoto_tests_with_grouped_fpd_format_mixed_agile_waterfall.py
@@ -0,0 +1,28 @@
+import unittest
+
+from src.fitters.fitter import GroupedFailuresPerDayFitter
+
+
+class GoelOkumotoTestsWithGroupedFailuresPerDayFormatMixedWaterfallAgileData(unittest.TestCase):
+
+    fit = None
+
+    @classmethod
+    def setUpClass(cls):
+        cls.fit = GroupedFailuresPerDayFitter().fit('goel-okumoto', 'mixed-waterfall-agile')
+
+    def test_mixed_waterfall_agile_goel_okumoto_maximum_likelihood_a_parameter_is_1373_comma_290255(self):
+        a = GoelOkumotoTestsWithGroupedFailuresPerDayFormatMixedWaterfallAgileData.fit.get_ml_parameters()[0]
+        self.assertAlmostEqual(a, 1373.290255, places=6)
+
+    def test_mixed_waterfall_agile_goel_okumoto_maximum_likelihood_b_parameter_is_0_comma_004957(self):
+        b = GoelOkumotoTestsWithGroupedFailuresPerDayFormatMixedWaterfallAgileData.fit.get_ml_parameters()[1]
+        self.assertAlmostEqual(b, 0.004957, places=6)
+
+    def test_mixed_waterfall_agile_goel_okumoto_maximum_likelihood_prr_is_9_comma_036989(self):
+        prr = GoelOkumotoTestsWithGroupedFailuresPerDayFormatMixedWaterfallAgileData.fit.get_prr_ml()
+        self.assertAlmostEqual(prr, 9.036989, places=6)
+
+    def test_mixed_waterfall_agile_goel_okumoto_aic_is_minus_777_comma_398430(self):
+        aic = GoelOkumotoTestsWithGroupedFailuresPerDayFormatMixedWaterfallAgileData.fit.get_aic()
+        self.assertAlmostEqual(aic, -777.398430, places=6)
