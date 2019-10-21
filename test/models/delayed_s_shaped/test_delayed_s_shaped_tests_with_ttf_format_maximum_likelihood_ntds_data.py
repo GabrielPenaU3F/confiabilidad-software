ff507113297754e6c2ce7fd5b6e939db6404046b
Gabriel Pena <gpena@untref.edu.ar>
2019-10-21 14:54:33 -0300

    Corrijo los tests del MTBF

--- test/models/delayed_s_shaped/test_delayed_s_shaped_tests_with_ttf_format_maximum_likelihood_ntds_data.py
+++ test/models/delayed_s_shaped/test_delayed_s_shaped_tests_with_ttf_format_maximum_likelihood_ntds_data.py
@@ -29,26 +29,26 @@ class DelayedSShapedTestsWithTTFFormatMaximumLikelihoodNtdsData(unittest.TestCas
 
     def test_mttf_for_k_equal_1_is_14_comma_4(self):
         mttf = DelayedSShapedTestsWithTTFFormatMaximumLikelihoodNtdsData.fit.get_mttf(1)
-        self.assertAlmostEqual(mttf, 14.4, places=1)
+        self.assertAlmostEqual(mttf, 14.4, delta=0.1)
 
     def test_mttf_for_k_equal_2_is_22_comma_6(self):
         mttf = DelayedSShapedTestsWithTTFFormatMaximumLikelihoodNtdsData.fit.get_mttf(2)
-        self.assertAlmostEqual(mttf, 22.6, places=1)
+        self.assertAlmostEqual(mttf, 22.6, delta=0.1)
 
     def test_mttf_for_k_equal_3_is_29_comma_3(self):
         mttf = DelayedSShapedTestsWithTTFFormatMaximumLikelihoodNtdsData.fit.get_mttf(3)
-        self.assertAlmostEqual(mttf, 29.3, places=1)
+        self.assertAlmostEqual(mttf, 29.3, delta=0.1)
 
     def test_mtbf_for_k_equal_1_is_14_comma_4(self):
         mtbf = DelayedSShapedTestsWithTTFFormatMaximumLikelihoodNtdsData.fit.get_mtbf(1)
-        self.assertAlmostEqual(mtbf, 14.4, places=1)
+        self.assertAlmostEqual(mtbf, 14.4, delta=0.1)
 
     def test_mtbf_for_k_equal_5_is_5_comma_8(self):
         mtbf = DelayedSShapedTestsWithTTFFormatMaximumLikelihoodNtdsData.fit.get_mtbf(5)
-        self.assertAlmostEqual(mtbf, 5.8, places=1)
+        self.assertAlmostEqual(mtbf, 5.8, delta=0.1)
 
     def test_mtbf_for_k_equal_20_is_9_comma_0(self):
         mtbf = DelayedSShapedTestsWithTTFFormatMaximumLikelihoodNtdsData.fit.get_mtbf(20)
-        self.assertAlmostEqual(mtbf, 9.0, places=1)
+        self.assertAlmostEqual(mtbf, 9.0, delta=0.1)
 
 
