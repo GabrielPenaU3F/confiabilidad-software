a9ede535b41bf58fde44984db7cdd2e032050efb
Gabriel Pena <gpena@untref.edu.ar>
2019-11-26 03:38:48 -0300

    Update script

--- scripts/papers_and_other_works/book_chapter/mixed_mtbf.py
+++ scripts/papers_and_other_works/book_chapter/mixed_mtbf.py
@@ -25,17 +25,28 @@ a_log = 835.4106
 b_log = 0.0307
 c_log = 75.8011
 
-mttf_go = go.calculate_mttfs(n, a_go, b_go)
-mttf_ds = ds.calculate_mttfs(n, a_ds, b_ds)
-mttf_log = log.calculate_mttfs(n, a_log, b_log, c_log)
+mttf_go = go.calculate_mttfs(n, a_go, b_go)[0:97]
+mttf_ds = ds.calculate_mttfs(n, a_ds, b_ds)[0:99]
+mttf_log = log.calculate_mttfs(n, a_log, b_log, c_log)[0:99]
 
 mtbf_go = go.calculate_mtbfs(mttf_go)
+mtbf_go.extend([0.1645, 0.1712, 0.1785, 0.1865, 0.1953, 0.205, 0.2155, 0.2273, 0.2405, 0.2553, 0.2672])
 mtbf_ds = ds.calculate_mtbfs(mttf_ds)
+mtbf_ds.extend([0.1459, 0.1407, 0.1399, 0.1422, 0.1470, 0.1545, 0.1651, 0.1793, 0.1985,
+                0.2250, 0.2640, 0.3240, 0.4330, 0.6860])
 mtbf_log = log.calculate_mtbfs(mttf_log)
+mtbf_log.extend([0.2661, 0.215, 0.1864, 0.1699, 0.1570, 0.1566, 0.1572, 0.1628, 0.1743, 0.1943, 0.2242])
 
-failures = np.linspace(1, len(mtbf_go), len(mtbf_go))
+failures_go = list(np.linspace(1, 98, 98))
+failures_ds = list(np.linspace(1, 100, 100))
+failures_log = list(np.linspace(1, 100, 100))
+failures_wolf = list(np.arange(150, 800, 50))
+failures_wolf2 = list(np.arange(150, 650, 50))
+failures_go.extend(failures_wolf2)
+failures_ds.extend(failures_wolf)
+failures_log.extend(failures_wolf2)
 
-ig, axes = plt.subplots()
+fig, axes = plt.subplots()
 axes.set_xlabel('Failure number')
 axes.set_ylabel('Mean time between failures')
 axes.set_xlim(left=0, auto=True)
@@ -45,11 +56,11 @@ axes.patch.set_edgecolor('black')
 axes.patch.set_linewidth('1')
 axes.set_facecolor("#ffffff")
 axes.grid(color='black', linestyle='--', linewidth=0.5)
-axes.plot(failures, mtbf_go,
+axes.plot(failures_go, mtbf_go,
           linewidth=1, color='#949494', linestyle='-', label='Goel-Okumoto')
-axes.plot(failures, mtbf_ds,
+axes.plot(failures_ds, mtbf_ds,
           linewidth=1, color='#696969', linestyle='-', label='Delayed S-Shaped')
-axes.plot(failures, mtbf_log,
+axes.plot(failures_log, mtbf_log,
           linewidth=2, color='#BDBDBD', linestyle='-', label='Logistic')
 
 axes.legend()
