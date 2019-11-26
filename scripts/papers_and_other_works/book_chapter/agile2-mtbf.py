731a869c05afc44b35334691479b577b3caa3718
Gabriel Pena <gpena@untref.edu.ar>
2019-11-26 20:43:09 -0300

    Update script Mixed-MTBF y Agile2-MTBF

--- scripts/papers_and_other_works/book_chapter/agile2-mtbf.py
+++ scripts/papers_and_other_works/book_chapter/agile2-mtbf.py
@@ -25,12 +25,19 @@ c_log = 580.9822
 mttf_ds = ds.calculate_mttfs(n, a_ds, b_ds)
 mttf_log = log.calculate_mttfs(n, a_log, b_log, c_log)
 
-mtbf_ds = ds.calculate_mtbfs(mttf_ds)
-mtbf_log = log.calculate_mtbfs(mttf_log)
+mtbf_ds = ds.calculate_mtbfs(mttf_ds)[0:19]
+mtbf_ds.extend([4.28, 3.715, 3.4060, 3.2180, 2.9440, 2.9790, 3.1310, 3.6680, 4.8290, 6.2600, 7.9800])
+mtbf_log = log.calculate_mtbfs(mttf_log)[0:99]
+mtbf_log.extend([2.916, 2.7720, 3.0520, 4.7880, 7.8900, 7.8400])
 
-failures = np.linspace(1, len(mtbf_ds), len(mtbf_ds))
+mtbf = log.calculate_mtbfs(mttf_log)
 
-ig, axes = plt.subplots()
+failures_ds = list(np.linspace(1, 19, 19))
+failures_ds.extend([20, 30, 40, 50, 100, 120, 150, 200, 250, 280, 300])
+failures_log = list(np.linspace(1, 99, 99))
+failures_log.extend([120, 150, 200, 250, 280, 300])
+
+fig, axes = plt.subplots()
 axes.set_xlabel('Failure number')
 axes.set_ylabel('Mean time between failures')
 axes.set_xlim(left=0, auto=True)
@@ -40,11 +47,10 @@ axes.patch.set_edgecolor('black')
 axes.patch.set_linewidth('1')
 axes.set_facecolor("#ffffff")
 axes.grid(color='black', linestyle='--', linewidth=0.5)
-axes.plot(failures, mtbf_ds,
+axes.plot(failures_ds, mtbf_ds,
           linewidth=1, color='#696969', linestyle='-', label='Delayed S-Shaped')
-axes.plot(failures, mtbf_log,
+axes.plot(failures_log, mtbf_log,
           linewidth=2, color='#BDBDBD', linestyle='-', label='Logistic')
-
 axes.legend()
 plt.show()
 
--- scripts/papers_and_other_works/book_chapter/mixed_mtbf.py
+++ scripts/papers_and_other_works/book_chapter/mixed_mtbf.py
@@ -30,21 +30,22 @@ mttf_ds = ds.calculate_mttfs(n, a_ds, b_ds)[0:99]
 mttf_log = log.calculate_mttfs(n, a_log, b_log, c_log)[0:99]
 
 mtbf_go = go.calculate_mtbfs(mttf_go)
-mtbf_go.extend([0.1645, 0.1712, 0.1785, 0.1865, 0.1953, 0.205, 0.2155, 0.2273, 0.2405, 0.2553, 0.2672])
+mtbf_go.extend([0.1645, 0.1712, 0.1785, 0.1865, 0.1953, 0.205, 0.2155, 0.2273, 0.2405, 0.2553, 0.2672,
+                0.2910, 0.3130, 0.3390])
 mtbf_ds = ds.calculate_mtbfs(mttf_ds)
 mtbf_ds.extend([0.1459, 0.1407, 0.1399, 0.1422, 0.1470, 0.1545, 0.1651, 0.1793, 0.1985,
                 0.2250, 0.2640, 0.3240, 0.4330, 0.6860])
 mtbf_log = log.calculate_mtbfs(mttf_log)
-mtbf_log.extend([0.2661, 0.215, 0.1864, 0.1699, 0.1570, 0.1566, 0.1572, 0.1628, 0.1743, 0.1943, 0.2242])
+mtbf_log.extend([0.2661, 0.215, 0.1864, 0.1699, 0.1570, 0.1566, 0.1572, 0.1628, 0.1743, 0.1943, 0.2242,
+                 0.2980, 0.4760, 0.7410])
 
 failures_go = list(np.linspace(1, 98, 98))
 failures_ds = list(np.linspace(1, 100, 100))
 failures_log = list(np.linspace(1, 100, 100))
 failures_wolf = list(np.arange(150, 800, 50))
-failures_wolf2 = list(np.arange(150, 650, 50))
-failures_go.extend(failures_wolf2)
+failures_go.extend(failures_wolf)
 failures_ds.extend(failures_wolf)
-failures_log.extend(failures_wolf2)
+failures_log.extend(failures_wolf)
 
 fig, axes = plt.subplots()
 axes.set_xlabel('Failure number')
