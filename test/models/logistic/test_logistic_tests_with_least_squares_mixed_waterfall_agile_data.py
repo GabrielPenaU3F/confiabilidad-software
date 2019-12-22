0f40a82cad20aa346bdc7f134ac4400bdf17d80e
Gabriel Pena <gpena@untref.edu.ar>
2019-12-21 22:52:15 -0300

    Modifico apenas los test del modelo Logístico; con la nueva implementación, algunos fallaban a partir del 4to decimal.

--- test/models/logistic/test_logistic_tests_with_least_squares_mixed_waterfall_agile_data.py
+++ test/models/logistic/test_logistic_tests_with_least_squares_mixed_waterfall_agile_data.py
@@ -9,21 +9,20 @@ class LogisticTestsWithLeastSquaresMixedWaterfallAgileData(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
-        cls.fit = Fitter().fit('logistic', 'mixed-waterfall-agile',
-                               initial_approx=(0.01, 0.001, 0.000001))
+        cls.fit = Fitter().fit('logistic', 'mixed-waterfall-agile', initial_approx=(0.01, 0.001, 0.00001))
 
-    def test_mixed_waterfall_agile_logistic_least_squares_a_parameter_is_835_comma_410609(self):
+    def test_mixed_waterfall_agile_logistic_least_squares_a_parameter_is_835_comma_410586(self):
         a = LogisticTestsWithLeastSquaresMixedWaterfallAgileData.fit.get_lsq_parameters()[0]
-        self.assertAlmostEqual(a, 835.410609, places=6)
+        self.assertAlmostEqual(a, 835.410586, places=6)
 
     def test_mixed_waterfall_agile_logistic_least_squares_b_parameter_is_0_comma_030741(self):
         b = LogisticTestsWithLeastSquaresMixedWaterfallAgileData.fit.get_lsq_parameters()[1]
         self.assertAlmostEqual(b, 0.030741, places=6)
 
-    def test_mixed_waterfall_agile_logistic_least_squares_c_parameter_is_0_comma_801129(self):
+    def test_mixed_waterfall_agile_logistic_least_squares_c_parameter_is_0_comma_801125(self):
         c = LogisticTestsWithLeastSquaresMixedWaterfallAgileData.fit.get_lsq_parameters()[2]
-        self.assertAlmostEqual(c, 75.801129, places=6)
+        self.assertAlmostEqual(c, 75.801125, places=6)
 
-    def test_mixed_waterfall_agile_logistic_least_squares_prr_is_7_comma_001084(self):
+    def test_mixed_waterfall_agile_logistic_least_squares_prr_is_7_comma_001083(self):
         prr = LogisticTestsWithLeastSquaresMixedWaterfallAgileData.fit.get_prr_lsq()
-        self.assertAlmostEqual(prr, 7.001084, places=6)
+        self.assertAlmostEqual(prr, 7.001083, places=6)
