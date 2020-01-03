d33c3e9b4f3275c3eb6982df797fccc59d90f56c
Gabriel Pena <gpena@untref.edu.ar>
2020-01-03 02:06:18 -0300

    Funciona el ajuste multietapa con dos etapas

--- src/domain/plotter/stage_plotter.py
+++ src/domain/plotter/stage_plotter.py
@@ -5,6 +5,10 @@ class StagePlotter(Plotter):
 
     def plot_results(self, axes, stage, times, cumulative_failures):
         plot_strategy = self.get_model_strategy(stage.get_model())()
+        initial_sample = self.determine_initial_sample(times, stage.get_initial_t())
+        end_sample = self.determine_end_sample(times, stage.get_end_t())
+        times = times[initial_sample:end_sample]
+        cumulative_failures = cumulative_failures[initial_sample:end_sample]
         plot_strategy.plot(axes, times, cumulative_failures, stage.get_lsq_params(), stage.get_ml_params())
 
     def plot_mttf(self, *args):
@@ -12,3 +16,13 @@ class StagePlotter(Plotter):
 
     def plot_mtbf(self, *args):
         pass
+
+    def determine_initial_sample(self, times, initial_t):
+        for i in range(len(times)):
+            if times[i] >= initial_t:
+                return i
+
+    def determine_end_sample(self, times, end_t):
+        for i in range(len(times)):
+            if times[i] >= end_t:
+                return i - 1
