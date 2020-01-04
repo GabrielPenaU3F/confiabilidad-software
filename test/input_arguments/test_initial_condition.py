b353474e86c0d026da9637662f570ba82c9b0709
Gabriel Pena <gpena@untref.edu.ar>
2020-01-04 20:44:20 -0300

    Actualizo algunos test de condiciones iniciales al nuevo comportamiento de t0

--- test/input_arguments/test_initial_condition.py
+++ test/input_arguments/test_initial_condition.py
@@ -17,19 +17,19 @@ class TestInitialCondition(unittest.TestCase):
         #  t0 = 5 in ttf format means '0 failures reported at t=5'
         #  t0 = 5 in fpd format means 'the dataset begins at t=6'
         cls.ttf_fit_ds = ModelFitter().fit('delayed-s-shaped', 'ntds', t0=5)
-        cls.fpd_fit_ds = ModelFitter().fit('delayed-s-shaped', 'mixed-waterfall-agile', t0=5)
+        # cls.fpd_fit_ds = ModelFitter().fit('delayed-s-shaped', 'mixed-waterfall-agile', t0=5)
         cls.ttf_fit_go = ModelFitter().fit('goel-okumoto', 'ntds', t0=5)
-        cls.fpd_fit_go = ModelFitter().fit('goel-okumoto', 'mixed-waterfall-agile', t0=5)
+        # cls.fpd_fit_go = ModelFitter().fit('goel-okumoto', 'mixed-waterfall-agile', t0=5)
         cls.ttf_fit_log = ModelFitter().fit('logistic', 'ntds', t0=5)
-        cls.fpd_fit_log = ModelFitter().fit('logistic', 'mixed-waterfall-agile', t0=5)
+        # cls.fpd_fit_log = ModelFitter().fit('logistic', 'mixed-waterfall-agile', t0=5)
 
-    def test_ntds_delayed_s_shaped_maximum_likelihood_a_parameter_with_t0_equal_5_is_27_comma_876270(self):
+    def test_ntds_delayed_s_shaped_maximum_likelihood_a_parameter_with_t0_equal_5_is_27_comma_572573(self):
         a = TestInitialCondition.ttf_fit_ds.get_ml_parameters()[0]
-        self.assertAlmostEqual(a, 27.876270, places=6)
+        self.assertAlmostEqual(a, 27.572573, places=6)
 
-    def test_ntds_delayed_s_shaped_maximum_likelihood_b_parameter_with_t0_equal_5_is_0_comma_017446(self):
+    def test_ntds_delayed_s_shaped_maximum_likelihood_b_parameter_with_t0_equal_5_is_0_comma_018700(self):
         b = TestInitialCondition.ttf_fit_ds.get_ml_parameters()[1]
-        self.assertAlmostEqual(b, 0.017446, delta=10**(-6))
+        self.assertAlmostEqual(b, 0.018700, delta=10**(-6))
 
     def test_mixed_delayed_s_shaped_maximum_likelihood_a_parameter_with_t0_equal_5_is_940_comma_279403(self):
         a = TestInitialCondition.fpd_fit_ds.get_ml_parameters()[0]
@@ -39,13 +39,13 @@ class TestInitialCondition(unittest.TestCase):
         b = TestInitialCondition.fpd_fit_ds.get_ml_parameters()[1]
         self.assertAlmostEqual(b, 0.021931, places=6)
 
-    def test_ntds_goel_okumoto_maximum_likelihood_a_parameter_with_t0_equal_5_is_34_comma_992027(self):
+    def test_ntds_goel_okumoto_maximum_likelihood_a_parameter_with_t0_equal_5_is_33_comma_529490(self):
         a = TestInitialCondition.ttf_fit_go.get_ml_parameters()[0]
-        self.assertAlmostEqual(a, 34.992027, places=6)
+        self.assertAlmostEqual(a, 33.529490, places=6)
 
-    def test_ntds_goel_okumoto_maximum_likelihood_b_parameter_with_t0_equal_5_is_0_comma_005790(self):
+    def test_ntds_goel_okumoto_maximum_likelihood_b_parameter_with_t0_equal_5_is_0_comma_006599(self):
         b = TestInitialCondition.ttf_fit_go.get_ml_parameters()[1]
-        self.assertAlmostEqual(b, 0.005790, places=6)
+        self.assertAlmostEqual(b, 0.006599, places=6)
 
     def test_mixed_goel_okumoto_maximum_likelihood_a_parameter_with_t0_equal_5_is_1414_comma_278491(self):
         a = TestInitialCondition.fpd_fit_go.get_ml_parameters()[0]
