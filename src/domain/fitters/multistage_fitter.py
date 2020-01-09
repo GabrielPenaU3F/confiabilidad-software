ee0bd77a6cd7508b37cfb0a3c7c29f11d6b77ac1
Gabriel Pena <gpena@untref.edu.ar>
2020-01-09 01:22:02 -0300

    Refactor al multistage fitter

--- src/domain/fitters/multistage_fitter.py
+++ src/domain/fitters/multistage_fitter.py
@@ -2,6 +2,7 @@ import numpy as np
 
 from src.data.data_repository import DataRepository
 from src.domain.fitters.fitter import Fitter
+from src.domain.fitters.stage_fitter import StageFitter
 from src.domain.optional_arguments import OptionalArguments
 from src.domain.stage import Stage
 from src.domain.multistage_fit import MultistageFit
@@ -19,65 +20,19 @@ class MultistageFitter(Fitter):
         self.stages.append(stage)
 
     def fit(self, project_name):
+        fitted_stages = []
         for i in range(len(self.stages)):
             stage = self.stages[i]
             fit_strategy = self.get_model_strategy(stage.get_model())(project_name)
             data = DataRepository.provide_project_data(project_name)
-            initial_sample = self.determine_initial_sample(data, stage.get_initial_t())
-            end_sample = self.determine_end_sample(data, stage.get_end_t())
-            t0 = self.determine_t0(data, stage.get_initial_t())
-            kwargs = {
-                'initial_sample': initial_sample,
-                'end_sample': end_sample,
-                't0': t0
-            }
-            optional_arguments = OptionalArguments(**kwargs)
-            try:
-                self.fit_stage(stage, fit_strategy, optional_arguments)
-            except InvalidFitException as error:
-                stage_number = i + 1
-                raise InvalidFitException('Stage ' + str(stage_number) + ': ' + error.strerror)
-        return MultistageFit(project_name, self.stages)
-
-    def fit_stage(self, stage, fit_strategy, optional_arguments):
-            stage_lsq_params, stage_ml_params = fit_strategy.fit_model(optional_arguments)
-            stage.set_lsq_params(stage_lsq_params)
-            stage.set_ml_params(stage_ml_params)
+            stage_number = i + 1
+            stage_fitter = StageFitter()
+            fitted_stage = stage_fitter.fit(stage, fit_strategy, data, stage_number)
+            fitted_stages.append(fitted_stage)
+        return MultistageFit(project_name, fitted_stages)
 
     def decode_kwargs(self, **kwargs):
         pass
-
-    def determine_end_sample(self, data, end_t):
-        format = data.get_format()
-        if format == 'ttf':
-            ttfs = data.get_data()
-            for i in range(len(ttfs)):
-                if ttfs[i] > end_t:
-                    return i - 1
-            return len(ttfs) - 1
-        elif format == 'grouped':
-            pass
-
-    def determine_initial_sample(self, data, initial_t):
-        format = data.get_format()
-        if format == 'ttf':
-            ttfs = data.get_data()
-            for i in range(len(ttfs)):
-                if ttfs[i] >= initial_t:
-                    return i
-        elif format == 'grouped':
-            pass
-
-    def determine_t0(self, data, initial_t):
-        format = data.get_format()
-        if format == 'ttf':
-            ttfs = list(np.copy(data.get_data()))
-            ttfs.insert(0, 0)
-            for i in range(len(ttfs)):
-                if ttfs[i] > initial_t:
-                    return ttfs[i - 1]
-        elif format == 'grouped':
-            pass
 
     def check_for_time_consistency(self, initial_t):
         if len(self.stages) > 0 and initial_t != self.stages[-1].get_end_t():
--- src/domain/stage.py
+++ src/domain/stage.py
@@ -4,8 +4,6 @@ class Stage:
         self.initial_t = initial_t
         self.end_t = end_t
         self.model = model
-        self.lsq_params = None
-        self.ml_params = None
 
     def get_initial_t(self):
         return self.initial_t
@@ -16,10 +14,15 @@ class Stage:
     def get_model(self):
         return self.model
 
-    def set_lsq_params(self, lsq_params):
-        self.lsq_params = lsq_params
+    def be_fitted(self, lsq_params, ml_params):
+        return FittedStage(self.initial_t, self.end_t, self.model, lsq_params, ml_params)
 
-    def set_ml_params(self, ml_params):
+
+class FittedStage(Stage):
+
+    def __init__(self, initial_t, end_t, model, lsq_params, ml_params):
+        super().__init__(initial_t, end_t, model)
+        self.lsq_params = lsq_params
         self.ml_params = ml_params
 
     def get_lsq_params(self):
@@ -27,3 +30,4 @@ class Stage:
 
     def get_ml_params(self):
         return self.ml_params
+
