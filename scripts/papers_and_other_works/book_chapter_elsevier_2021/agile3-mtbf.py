c57572ec3f9fba01331718d892d94d720cc5d04d
Gabriel Pena <gpena@untref.edu.ar>
2021-03-16 23:03:39 -0300

    Modifications on scripts

---
+++ scripts/papers_and_other_works/book_chapter_elsevier_2021/agile3-mtbf.py
@@ -0,0 +1,54 @@
+# Agile Project #1 in the book
+
+import numpy as np
+
+from src.data.data_repository import DataRepository
+from src.domain.fitters.model_fitter import ModelFitter
+from matplotlib import pyplot as plt
+from matplotlib import rc
+import matplotlib.font_manager
+
+rc('font', **{'family': 'serif', 'serif': ['CMU Sans Serif']})
+plt.rcParams['font.family'] = 'Calibri'
+
+ttf_fitter = ModelFitter()
+ds_fit = ttf_fitter.fit('delayed-s-shaped', 'agile-n3', initial_approx=(100, 0.001),
+                        lsq_only=True, mt_formula='regular')
+log_fit = ttf_fitter.fit('logistic', 'agile-n3', initial_approx=(30, 0.03, 300),
+                         lsq_only=True, mt_formula='regular')
+bc_fit = ttf_fitter.fit('barraza-contagion', 'agile-n3',
+                        lsq_only=True, mt_formula='conditional')
+
+data = DataRepository.provide_project_data('agile-n3')
+real_tbf = data.get_time_between_failures()
+x_axis_data = np.linspace(1, len(real_tbf), len(real_tbf))
+
+ds_mtbf = ds_fit.get_all_mtbf()
+log_mtbf = log_fit.get_all_mtbf()
+bc_mtbf = bc_fit.get_all_mtbf()
+
+fig, axes = plt.subplots(figsize=(6, 4))
+axes.set_xlabel('Failure number', fontsize=12)
+axes.xaxis.set_tick_params(labelsize=10)
+axes.xaxis.labelpad = 4
+axes.set_ylabel('Mean time between failures', fontsize=12)
+axes.yaxis.set_tick_params(labelsize=10)
+axes.yaxis.labelpad = 8
+axes.set_xlim(left=0, auto=True)
+axes.set_ylim(auto=True)
+axes.patch.set_facecolor("#ffffff")
+axes.patch.set_edgecolor('black')
+axes.patch.set_linewidth('1')
+axes.set_facecolor("#ffffff")
+axes.grid(color='black', linestyle='--', linewidth=0.5)
+axes.scatter(x_axis_data, real_tbf, linewidth=1, color='#263859', s=4.0,
+          label='Real data (Project #1)')
+axes.plot(x_axis_data, ds_mtbf,
+          linewidth=1, color='#ca3e47', linestyle='-', label='Delayed S-shaped')
+axes.plot(x_axis_data, log_mtbf,
+          linewidth=1, color='#58b368', linestyle='-', label='Logistic')
+axes.plot(x_axis_data, bc_mtbf,
+          linewidth=1, color='#4a47a3', linestyle='-', label='Linear contagion')
+axes.legend(fontsize=12)
+
+plt.savefig('fig1.tiff', dpi=300)
